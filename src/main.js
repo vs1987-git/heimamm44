dc5ec7cf494662c4a3e6aeb53cf6a4ee46fddc5b
vs1987-git <haihaojx@sina.com>
2019-12-13 20:05:30 +0800

    项目起步 - Element-ui整合

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 //导入接收路由
 import router from './router/router.js'
+
+// 导入elementUi
+import ElementUI from 'element-ui'
+// 导入elementUI 样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+//注册一下
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
